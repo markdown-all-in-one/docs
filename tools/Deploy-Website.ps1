6e710ab49cc80a34eee3ad56098f6ff5e5137609
Lemmingh <43396014+Lemmingh@users.noreply.github.com>
2020-08-03 12:13:44 +0800

    👷 Fix "Deploy-Website.ps1"
    * Fix `Remove-Item -Path "$Destination/*"`
    * Set Git identity properly.
    * Beautify log output.
    * Add environment check.

--- tools/Deploy-Website.ps1
+++ tools/Deploy-Website.ps1
@@ -1,3 +1,10 @@
+<#!
+ # Copyright (c) Lemming <https://github.com/Lemmingh>
+ # Licensed under the ISC License.
+ #>
+
+#Requires -Version 7
+
 [CmdletBinding()]
 param (
     [string]
@@ -19,26 +26,101 @@ param (
     $BuildTimeUtc = [datetime]::UtcNow
 )
 
+<# Check environment. #>
+
+if ($env:GITHUB_ACTIONS -ne 'true')
+{
+    Write-Warning 'This script is designed to run on GitHub Actions.'
+}
+
+if (-not $IsLinux)
+{
+    Write-Warning 'This script is designed to run on Linux.'
+}
+
+<# Prepare. #>
+
 [string]$Timestamp = $BuildTimeUtc.ToString('o')
+
+$InvokeTaskGroup = {
+    param (
+        [string]$Title,
+        [scriptblock]$Command
+    )
+
+    Write-Output "::group::$Title"
+    . $Command # I don't want to create too deeply nested scopes.
+    Write-Output '::endgroup::'
+}
+
+Set-Variable -Name @('Timestamp', 'InvokeTaskGroup') -Option ReadOnly
+
+$Global:isGithubDebug = [bool]$env:RUNNER_DEBUG
+
+function Invoke-DebugCommand ([string]$Title, [scriptblock]$Command)
+{
+    if ($Global:isGithubDebug -or [bool]$env:RUNNER_DEBUG)
+    {
+        Write-Output "::group::[debug]$Title"
+        & $Command
+        Write-Output '::endgroup::'
+    }
+}
+
+# Note:
+# `$InvokeTaskGroup` is designed to run commands in the same scope as caller. Please dot sourcing it.
+# Function `Invoke-DebugCommand` is designed to run debug-purpose commands in a child scope to minimize effects on caller.
+
+<# Main. #>
+
+. $InvokeTaskGroup -Title 'Arguments' -Command {
+    Get-Variable -Name @('Source', 'Destination', 'Sha', 'Timestamp') | Format-Table -AutoSize -Wrap # `Timestamp` is clearer than `$BuildTimeUtc.ToString()`.
+}
 
 Set-Location -Path $Destination
 
-Write-Verbose 'Empty the destination.'
-Remove-Item -Path $Destination -Exclude @('.git*', '.nojekyll') -Recurse
+Invoke-DebugCommand -Title 'Pre-Run: Empty the destination' -Command {
+    Write-Output '::debug::Destination directory contents:'
+    ls -l -A
+    Write-Output '::debug::Remove-Item -WhatIf:'
+    Remove-Item -Path "$Destination/*" -Exclude @('.git*', '.nojekyll') -Recurse -WhatIf
+}
 
-Write-Output "::debug::Remaining entries:`n$($(ls -l -A) -join "`n")"
+. $InvokeTaskGroup -Title 'Empty the destination' -Command {
+    Remove-Item -Path "$Destination/*" -Exclude @('.git*', '.nojekyll') -Recurse -ErrorAction:Continue
+}
 
-Write-Output '::group::Copy files to the destination.'
-Copy-Item -Path "$Source/*" -Destination $Destination -Recurse -PassThru
-Write-Output '::endgroup::'
+Invoke-DebugCommand -Title 'Remaining entries' -Command {
+    Write-Output '::debug::Remaining entries:'
+    ls -l -A
+}
+
+. $InvokeTaskGroup -Title 'Copy files to the destination' -Command {
+    Copy-Item -Path "$Source/*" -Destination $Destination -Recurse -PassThru | Where-Object { $_ -is [System.IO.FileInfo] }
+}
+# If `FileInfo` and `DirectoryInfo` were mixed together, `Format-List` seemed to be called. I don't know why.
+# ? Should we install `tree` to list contents?
 
 # ! Security concern: Mask everything potential in workflow file before debugging!
-Write-Output "::debug::Git configuration:`n$($(git config --list --show-scope) -join "`n")"
+Invoke-DebugCommand -Title 'Git configuration' -Command {
+    Write-Output '::debug::Git configuration:'
+    git config --list --show-scope
+    Write-Output '::debug::Git remotes:'
+    git remote -v
+}
 
-Write-Output '::group::Push to remote repository.'
-# Use the default config of the repository.
-# This script should work with <https://github.com/marketplace/actions/checkout>.
-git add -A
-git commit -m "Build $Sha at $Timestamp"
-git push
-Write-Output '::endgroup::'
+. $InvokeTaskGroup -Title 'Push to remote repository' -Command {
+    # Use the default config of the repository.
+    # This script should work with <https://github.com/marketplace/actions/checkout>.
+
+    Write-Output 'Set Git identity.'
+    git config user.email 'noreply@github.com'
+    git config user.name 'GitHub'
+
+    Write-Output 'Commit changes.'
+    git add -A
+    git commit -m "Build $Sha at $Timestamp"
+
+    Write-Output 'Push.'
+    git push
+}

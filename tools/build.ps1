c488c06145b8e77e92d5fbdabf7ee040613f72c4
Lemmingh <43396014+Lemmingh@users.noreply.github.com>
2020-10-22 01:14:04 +0800

    👷 Separate build script

---
+++ tools/build.ps1
@@ -0,0 +1,23 @@
+<#!
+ # Copyright (c) Lemming <https://github.com/Lemmingh>
+ # Licensed under the ISC License.
+ #>
+
+#Requires -Version 7
+
+<# Prepare. #>
+
+[string]$WorkingDirectoryPath = Split-Path -Path $PSScriptRoot -Parent
+[string]$PackageManifestUrl = 'https://raw.githubusercontent.com/yzhang-gh/vscode-markdown/master/package.json'
+[string]$PackageNlsUrl = 'https://raw.githubusercontent.com/yzhang-gh/vscode-markdown/master/package.nls.json'
+
+Set-Variable -Name @('WorkingDirectoryPath', 'PackageManifestUrl', 'PackageNlsUrl') -Option ReadOnly
+
+Set-Location -Path $WorkingDirectoryPath -PassThru
+
+# https://devblogs.microsoft.com/nuget/deprecating-tls-1-0-and-1-1-on-nuget-org/
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12 -bor [System.Net.SecurityProtocolType]::Tls13
+
+<# Main. #>
+
+npm run docs:build

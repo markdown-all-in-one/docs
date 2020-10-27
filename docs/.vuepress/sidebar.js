8ad42aa827cd739204ec583d8215b9484fba3d2e
Lemmingh <43396014+Lemmingh@users.noreply.github.com>
2020-10-27 13:57:06 +0800

    ✨ Finish "Packaging"

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// The most `README` here are just covers. Don't show them in sidebar. Exceptions:
+// * '/guide/'
+
 module.exports = {
     '/decisions/': [
         // Sort by attention.
@@ -18,14 +21,75 @@ module.exports = {
         'performance',
     ],
     '/project-management/docs/': [
-        // The `README` here is just a cover. Don't show it in sidebar.
+        // Sort in alphabetical order.
         'labels',
         'life-cycle',
         'writing-style-guide',
     ],
-    //// '/project-management/main/': [],
-    //// '/specs/': [
-    ////     // Sort in alphabetical order.
-    //// ],
+    '/project-management/main/': [
+        // Sort in alphabetical order.
+        {
+            title: 'Automation',
+            children: [
+                'automation/ci',
+            ]
+        },
+        'code-style',
+        'emoji',
+        'issue',
+        'labels',
+        'life-cycle',
+        'packaging',
+        'repository-organization',
+        'test',
+    ],
+    '/specs/': [
+        // Sort in alphabetical order.
+        {
+            title: 'List editing',
+            children: [
+                'list-editing/list-continuation',
+                'list-editing/list-toggling',
+            ]
+        },
+        {
+            title: 'Markdown table',
+            children: [
+                'markdown-table/table-formatting',
+            ]
+        },
+        {
+            title: 'Math',
+            children: [
+                'math/definitions-of-math-areas',
+                'math/math-environment-toggling',
+                'math/math-syntax-highlighting',
+            ]
+        },
+        {
+            title: 'Slugify',
+            children: [
+                'slugify/azure-devops',
+                'slugify/gitea',
+                'slugify/github-enterprise-server',
+                'slugify/github',
+                'slugify/gitlab',
+                'slugify/hugo',
+                'slugify/vscode',
+            ]
+        },
+        {
+            title: 'Table of contents',
+            children: [
+                'table-of-contents/toc-detection',
+                'table-of-contents/toc-generation',
+            ]
+        },
+        'code-completion',
+        'exporting-to-html',
+        'markdown-preview',
+        'pasting-link',
+        'syntax-decorations',
+    ],
     '/': 'auto' // Fallback.
 };
